f4e3968333682809026a8b20ea8267b5d9819595
JUAN MARTINEZ <163062478+juanmartinezjorcin@users.noreply.github.com>
2025-05-14 19:58:56 -0300

    lo que hay

---
+++ cuestionario/src/pages/home.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const Home = () => {
+return(
+<div>
+    <h1>Home</h1>
+    <input placeholder = "ingrece su usuario"></input>
+    {/* <button onClick={link='./home'}>Ingresar</button> */}
+</div>
+)
+}
+export default Home;
---
+++ cuestionario/src/pages/login.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+ 
+const Login = () => {
+return(
+<div>
+    <h1>login</h1>
+    <input placeholder = "ingrece su usuario"></input>
+    {/* <button onClick={link='./home'}>Ingresar</button> */}
+</div>
+)
+}
+export default Login;
