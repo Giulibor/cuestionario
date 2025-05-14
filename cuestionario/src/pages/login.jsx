f5e9c2289d14eeb3d5c9ce1e0f07b7df1e123718
Giuliana Bordon Castro <giuliana.bordon@correo.ucu.edu.uy>
2025-05-14 20:34:35 -0300

    css

--- cuestionario/src/pages/login.jsx
+++ cuestionario/src/pages/login.jsx
@@ -1,13 +1,19 @@
-import React from "react"
+import React from "react";
+import '../components/login.css';
+import {Link} from "react-router-dom";
 
- 
 const Login = () => {
-return(
-<div>
-    <h1>login</h1>
-    <input placeholder = "ingrece su usuario"></input>
-    {/* <button onClick={link='./home'}>Ingresar</button> */}
-</div>
-)
-}
+  return (
+    <div className="login-wrapper">
+      <div className="login-container">
+        <h1>Login</h1>
+        <input placeholder="Ingrese su usuario" />
+        <Link to="./home">
+            <button className="login-button">Ingresar</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
 export default Login;
